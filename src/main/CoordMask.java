07e9072f51b6091283cfe61b36db66abdef6f236
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-06 11:59:47 +0200

    CoordMask L_FORM impl

--- src/main/CoordMask.java
+++ src/main/CoordMask.java
@@ -73,7 +73,52 @@ public enum CoordMask {
                     return ret;
                 }
             }
-    );
+    ),
+
+    /* Кирпичик     []
+     *          [][][]
+     */
+    L_FORM(
+            new GenerationDelegate() {
+                @Override
+                public Coord[] generateFigure(Coord initialCoord, RotationMode rotation) {
+                    Coord[] ret = new Coord[4];
+
+                    switch (rotation){
+                        case NORMAL:
+                            ret[0] = initialCoord;
+                            ret[1] = new Coord(initialCoord.x, initialCoord.y - 1);
+                            ret[2] = new Coord(initialCoord.x, initialCoord.y - 2);
+                            ret[3] = new Coord(initialCoord.x + 1, initialCoord.y - 2);
+                            break;
+                        case INVERT:
+                            ret[0] = initialCoord;
+                            ret[1] = new Coord(initialCoord.x + 1, initialCoord.y);
+                            ret[2] = new Coord(initialCoord.x + 1, initialCoord.y - 1);
+                            ret[3] = new Coord(initialCoord.x + 1, initialCoord.y - 2);
+                            break;
+                        case FLIP_CCW:
+                            ret[0] = new Coord(initialCoord.x, initialCoord.y - 1);
+                            ret[1] = new Coord(initialCoord.x + 1, initialCoord.y - 1);
+                            ret[2] = new Coord(initialCoord.x + 2, initialCoord.y - 1);
+                            ret[3] = new Coord(initialCoord.x + 2, initialCoord.y);
+                            break;
+                        case FLIP_CW:
+                            ret[0] = new Coord(initialCoord.x, initialCoord.y - 1);
+                            ret[1] = initialCoord;
+                            ret[2] = new Coord(initialCoord.x + 1, initialCoord.y);
+                            ret[3] = new Coord(initialCoord.x + 2, initialCoord.y);
+                            break;
+                        default:
+                            ErrorCatcher.wrongParameter("rotation", "CoordMask");
+                    }
+
+                    return ret;
+                }
+            }
+    ),
+
+
 
     /**
      * Делегат, содержащий метод,
